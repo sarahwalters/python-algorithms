5884e76510d4e619c1116563a2c8918cd8267a8a
Sarah Walters <sarah.walters@students.olin.edu>
2015-09-30 16:21:20 -0400

    BST and augmented BST

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -1,4 +1,9 @@
 class BST:
+  ''' Maintains an unbalanced binary tree of comparable elements. All nodes in
+      the left subtree of a node have data smaller than the node's datum. All
+      nodes in the right subtree have data greater than or equal to the node's datum.
+      Interface with the tree using insert and search. '''
+
   def __init__(self, data):
     self.root = None
     for datum in data:
@@ -34,7 +39,7 @@ class BST:
     while True:
       if datum == current.datum:
         print "%s found in tree after %s steps" % (datum, steps)
-        return datum
+        return current
       elif datum < current.datum:
         if not current.left_child:
           print "%s not in tree after %s steps" % (datum, steps)
@@ -48,6 +53,43 @@ class BST:
         current = current.right_child
         steps = steps + 1
 
+  def traverse_in_order(self, current=None):
+    current = current if current else self.root
+    if current.left_child:
+      self.traverse_in_order(current.left_child)
+    current.display()
+    if current.right_child:
+      self.traverse_in_order(current.right_child)
+
+
+class AugmentedBST(BST):
+  ''' A BST augmented with the rank of each node, where the rank is the number of
+      data in the tree which are <= the node's datum. '''
+
+  def __init__(self, data):
+    BST.__init__(self, data)
+
+  def insert(self, datum):
+    if not self.root:
+      self.root = AugmentedBSTNode(datum)
+      return
+    else:
+      current = self.root
+      while True:
+        if current.datum > datum: # go left
+          current.increment_rank()
+          if current.left_child:
+            current = current.left_child
+          else:
+            current.left_child = AugmentedBSTNode(datum, current.rank - 1)
+            return
+        else: # go right
+          if current.right_child:
+            current = current.right_child
+          else:
+            current.right_child = AugmentedBSTNode(datum, current.rank + 1)
+            return
+
 
 class BSTNode:
   def __init__(self, datum):
@@ -56,3 +98,24 @@ class BSTNode:
     # starts out as a leaf
     self.left_child = None
     self.right_child = None
+
+  def display(self):
+    print self.datum
+
+
+class AugmentedBSTNode(BSTNode):
+  def __init__(self, datum, rank=1):
+    BSTNode.__init__(self, datum)
+    self.rank = rank # how many data in the tree are <= this node's datum
+
+  def increment_rank(self):
+    self.rank = self.rank + 1
+
+    # Also increment the rank of everything in the right subtree
+    if self.right_child:
+      self.right_child.increment_rank()
+      if self.right_child.left_child:
+        self.right_child.left_child.increment_rank()
+
+  def display(self):
+    print 'Rank %s: %s' % (self.rank, self.datum)
